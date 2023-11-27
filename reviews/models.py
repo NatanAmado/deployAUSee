dd26f2a2130fb23096835e246b369b2ba1516309
ovi <ovirandunu@gmail.com>
2023-11-27 13:11:26 +0100

    go

--- reviews/models.py
+++ reviews/models.py
@@ -50,6 +50,7 @@ class Review(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    
 
 
 
